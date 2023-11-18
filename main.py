d993af5e2617130b8b81621a1eb8d8a959166b29
MRLIS <elisey.d.zinovyev@gmail.com>
2023-11-18 13:07:35 +0300

    КРУГИ Желтенькие

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+import sys
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 200, 200)
+        self.setWindowTitle('Желтые круги')
+        self.btn = QPushButton('Рисовать', self)
+        self.btn.move(70, 150)
+        self.btn.resize(60, 40)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawflag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def drawflag(self, qp):
+        a = random.randint(10, 100)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(random.randint(0, 200), random.randint(0, 200), a, a)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
+
